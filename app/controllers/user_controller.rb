65f40072c68526fbd88c1bbb4431f85427bb2508
FeelThisMoment <bear_cat@ukr.net>
2021-07-13 23:36:34 +0300

    7.3 Unsuccessful signups    errors  with  name of controller

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -6,4 +6,19 @@ class UserController < ApplicationController
   def new
     @user = User.new
   end
+  def create
+    @user = User.new(user_params)   # Не кінцева реалізація !
+    if @user.save
+      # Опрацювати успішне збереження.
+    else
+      render 'new'
+    end
+  end
+  
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
+  end
 end
